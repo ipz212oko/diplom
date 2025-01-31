80dab68dd01db8887a56096d7c32b67ee6ee5b5e
Kateryna <ipz212_oko@student.ztu.edu.ua>
2025-01-31 17:54:38 +0200

    [FEATURE] login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { DefaultLayout } from "@/layouts/DefaultLayout.jsx";
 import { Main } from "@/pages/Main";
 import { NotFound } from "@/pages/NotFound.jsx";
 import { SignUp } from "@/pages/SignUp.jsx";
+import { Login } from "@/pages/Login.jsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<DefaultLayout/>}>
         <Route index element={<Main/>} />
         <Route path="sign-up" element={<SignUp/>} />
+        <Route path="login" element={<Login/>} />
         <Route path="not-found" element={<NotFound/>}/>
       </Route>
       <Route path="*" element={<Navigate replace to="/not-found" />} />
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,41 @@
+import * as yup from "yup"
+import { Center, Card, Heading, Button, Stack } from "@chakra-ui/react";
+import { SegmentedControl } from "@/components/ui/segmented-control.jsx";
+import { InputField } from "@/components/ui/field.jsx";
+import { Form } from "@/components/ui/form.jsx";
+import { validationMessage } from "@/config/validationMessage.js";
+import { FieldController } from "@/components/ui/field-controller.jsx";
+
+const { REQUIRED, INVALID_EMAIL } = validationMessage;
+
+const validationSchema = yup
+  .object({
+    email: yup.string().email(INVALID_EMAIL).required(REQUIRED),
+    password: yup.string().required(REQUIRED),
+  })
+  .required()
+
+export function Login() {
+  const onSubmit = data => {
+    console.log(data);
+  }
+
+  return (
+    <Center flex="1">
+      <Card.Root size="md" maxW="400px" w="full">
+        <Form onSubmit={onSubmit} validation={validationSchema}>
+          <Card.Header as={Heading} textAlign="center" size="3xl">Вхід</Card.Header>
+          <Card.Body>
+              <Stack spacing={2} alignItems="center">
+                <InputField label="Пошта" name="email" />
+                <InputField label="Пароль" name="password" />
+              </Stack>
+          </Card.Body>
+          <Card.Footer justifyContent="center">
+            <Button type="submit" colorPalette="blue">Увійти</Button>
+          </Card.Footer>
+        </Form>
+      </Card.Root>
+    </Center>
+  )
+}
