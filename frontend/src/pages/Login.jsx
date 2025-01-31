f97eb969974371f3947c4acf7485e6419f3a99d4
Kateryna <ipz212_oko@student.ztu.edu.ua>
2025-01-31 17:55:42 +0200

    [REFACTOR] remove unused imports

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,8 @@
 import * as yup from "yup"
 import { Center, Card, Heading, Button, Stack } from "@chakra-ui/react";
-import { SegmentedControl } from "@/components/ui/segmented-control.jsx";
 import { InputField } from "@/components/ui/field.jsx";
 import { Form } from "@/components/ui/form.jsx";
 import { validationMessage } from "@/config/validationMessage.js";
-import { FieldController } from "@/components/ui/field-controller.jsx";
 
 const { REQUIRED, INVALID_EMAIL } = validationMessage;
 
