48236476b7f58902e18399d8d82c3002425eb3d1
Kateryna <ipz212_oko@student.ztu.edu.ua>
2025-02-09 08:19:27 +0200

    [FEATURE] add region to user

--- api/src/server.js
+++ api/src/server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 async function syncDatabase() {
   try {
-    await sequelize.sync({ force: false });
+    await sequelize.sync({ alter: true });
     console.log('All tables have been created or are already existing.');
   } catch (error) {
     console.error('Error synchronizing models:', error);
